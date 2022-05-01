9b7c4a65add738aece4c8d11cbe5205c601281b5
Gene Calmer <yingyangforce@gmail.com>
2022-04-30 21:05:17 -0400

    FEAT: add Push()

--- grace-hw4.c
+++ grace-hw4.c
@@ -6,7 +6,32 @@
 
 #include <stdio.h>
 
+void Push(int **SP, int val);
+int Pop(int **SP);
+int Instack(int *SP, int val);
+int SizeofStack(int *SP);
+void Peek(int *SP);
+void PrintStack(int *SP);
+
+int Stack[100];
+int *SP = Stack;            //Moving stack pointer
+const int *InitSP = Stack;  //Pointer to start of stack
+
+//Fns ---
+
 int main() {
-    printf("nice\n");
+    printf("prepush -> %d\n", Stack[0]);
+    printf("Stackpointer add -> %p\n", &SP);
+    printf("Stackpointer val -> %p\n", SP);
+    Push(&SP, 12);
+    printf("post push -> %d\n", Stack[0]);
+    printf("Stackpointer add -> %p\n", &SP);
+    printf("Stackpointer val -> %p\n", SP);
     return 0;
 }
+
+void Push(int **SP, int val) {
+    **SP = val; //set current top of stack to val
+    *SP = *SP + 1; //move pointer to new top of stack
+}
+
